c8d1599fa6d4a2227994df35952168395fb63b6d
salockhart <salexlockhart@gmail.com>
2019-02-24 15:41:57 +0000

    cleanup

--- app.js
+++ app.js
@@ -27,20 +27,4 @@ app.use(fathom({ server: process.env['FATHOM_SERVER'], siteID: process.env['FATH
 
 app.use('/api/apps', appsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
