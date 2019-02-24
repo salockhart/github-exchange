e28743fb9e7a92015d35b6d1d6145c5026a9a85d
salockhart <salexlockhart@gmail.com>
2019-02-24 15:42:36 +0000

    cleanup

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const fathom = require('express-fathom');
 
 const appsRouter = require('./routes/apps');
@@ -21,7 +17,6 @@ if (process.env['NODE_ENV'] !== 'production') {
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use(fathom({ server: process.env['FATHOM_SERVER'], siteID: process.env['FATHOM_SITE'] }))
 
